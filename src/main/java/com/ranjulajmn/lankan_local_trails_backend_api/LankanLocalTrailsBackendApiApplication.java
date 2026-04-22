8bd2954a2b1f2d42dcc51b4f29e937d8473c4954
RanjulaJMN <ranjulajmn41@gmail.com>
2026-04-22 23:48:50 +0530

    Initial commit for lanakan_local_trails_backend_db

---
+++ src/main/java/com/ranjulajmn/lankan_local_trails_backend_api/LankanLocalTrailsBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.ranjulajmn.lankan_local_trails_backend_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LankanLocalTrailsBackendApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LankanLocalTrailsBackendApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ranjulajmn/lankan_local_trails_backend_api/LankanLocalTrailsBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranjulajmn.lankan_local_trails_backend_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LankanLocalTrailsBackendApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
